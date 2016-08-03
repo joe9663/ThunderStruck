e7b2ed1a6a770b4c5845ad13d7ed068f1c4c7139
Mason Brown & Joe Wood <pair+codingmason.joe9663@devbootcamp.com>
2016-08-03 15:23:51 -0400

    Create a user game join table

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,7 +1,12 @@
 class Game < ApplicationRecord
-  has_many :users
+  has_many :game_users
+  has_many :users, through: :game_users
   has_many :shots
   has_many :ships
-
+  has_many :destroyers
+  has_many :bonnscotts
+  has_many :batteships
+  has_many :cruisers
+  has_many :patrol_boats
 
 end
