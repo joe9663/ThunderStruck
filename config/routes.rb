6c251e7223dfcef1247cbee8ef77eac0fd443cd6
Ben Kudler <bkudler@gmail.com>
2016-08-03 17:01:31 -0400

    finishes login and register

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :games
 
-  root 'users#new'
+  root 'users#index'
 
-  resources :users, except: [:destroy, :index]
+  resources :users, except: :destroy
 
+  resources :sessions, only: [:create, :destroy, :new]
 
 end
